4e682c80d5d45b312bb4acc316f362759469ed64
Anand <achaudharyskn@gmail.com>
2024-09-06 22:33:31 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,54 @@
-import React, { useEffect, useState, useMemo, lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState, useMemo, lazy, Suspense, useContext } from 'react';
+import { CountContext,SetCountContext } from './Context';
 
-const Dashboard = lazy(() => import('./component/Dashboard'));
-const Landing = lazy(() => import('./component/landing'));
-const Appbar = lazy(() => import('./component/Appbar'));
-
-// Your component logic here...
-
-
+//first create a coxtext   api then wrap anyone thats want to use the telelported value inside a provider 
 function App(){
-  return  (
-   <div> 
-  <div>
-   <BrowserRouter>
-   <Appbar></Appbar>
-     <Routes>
-        <Route path='/dashboard' element={<Suspense fallback = {"loading.."}><Dashboard/></Suspense>}></Route>
-        <Route path='/landingPage' element={<Suspense fallback = {"loading.."}><Landing/></Suspense>}></Route>
-      </Routes>
-   </BrowserRouter>
-   </div>
-  </div>
+  const[count,setCount] = useState(0)
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+      <SetCountContext.Provider value={setCount}>
+      <Count  ></Count>
+      </SetCountContext.Provider>
+      </CountContext.Provider>
+
+    </div>
   )
+
 } 
+
+function Count(){
+  const setCount = useContext(SetCountContext)
+  return <div>
+  <CountRenderer></CountRenderer>
+   <Buttons  setCount= {setCount} ></Buttons> 
+  </div>
+
+}
+function CountRenderer (){
+  const count = useContext(CountContext)
  
+  
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons(){
+  const count = useContext(CountContext)
+  const setCount = useContext(SetCountContext)
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1);
+    }}>Increase</button>
+
+
+   <button onClick={()=>{
+     setCount(count-1)
+    }}>Decrease</button>
+  </div>
+}
+
 
 export default App;
 
